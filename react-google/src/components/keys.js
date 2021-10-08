91ed2a204ff337dbb65a75119bdaba827eb83f4f
nitace1 <ns650000@gmail.com>
2021-10-08 20:37:51 +0530

    Google clone

---
+++ react-google/src/components/keys.js
@@ -0,0 +1,2 @@
+export const API_KEY = "AIzaSyDlQ0Y5D9HpQBqJyaFzc6EXJTfcTU9ixl0";
+export default API_KEY;
---
+++ react-google/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { StateProvider } from "./components/StateProvider";
+import reducer, { initialState } from "./reducer";
+import "./index.css";
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
